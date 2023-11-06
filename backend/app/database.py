b97628ec3db0d1e11be476955bc421cb0ca3d387
Gabriel <gabriel@gabrielsilveira.com.br>
2023-11-06 20:10:05 -0300

    db_url string variable

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,11 +5,10 @@ from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://"+config.DB_USER+":"+config.DB_PASS+"@"+config.DB_HOST+":"+config.DB_PORT+"/"+config.DB_NAME
+db_url = "mysql+mysqldb://"+config['DB_USER']+":"+config['DB_PASS']+"@"+config['DB_HOST']+":"+config['DB_PORT']+"/"+config['DB_NAME']
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(db_url, connect_args={"check_same_thread": False})
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
