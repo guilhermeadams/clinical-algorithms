91e7bbb5169302bb6560088597633c3929e96795
Gabriel <gabriel@gabrielsilveira.com.br>
2023-11-07 12:56:19 -0300

    New project structure

---
+++ backend/services/algorithms_service.py
@@ -0,0 +1,21 @@
+from models.index import algorithms
+from db import conn
+
+
+def get_algorithms():
+    all_algorithms = conn.execute(algorithms.select()).fetchall()
+
+    items = []
+    item = {}
+    for algorithm in all_algorithms:
+        item = {
+            'id': algorithm[0],
+            'title': algorithm[1],
+            'description': algorithm[2],
+            'author': algorithm[3],
+            'version': algorithm[4]
+        }
+        items.append(item)
+        item = {}
+
+    return items
