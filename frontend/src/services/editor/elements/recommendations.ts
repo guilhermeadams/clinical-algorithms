22b48d66b7225c6a416beab135dd0e33acc7a8e8
Gabriel <gabriel@gabrielsilveira.com.br>
2024-01-03 15:30:58 -0300

    Direction and strength icons on right panel

---
+++ frontend/src/services/editor/elements/recommendations.ts
@@ -0,0 +1,38 @@
+import { DIRECTIONS, STRENGTH } from 'src/services/editor/constants';
+
+export const getDirectionIcon = (
+  direction: string,
+  params?: { iconWidth?: number, labelSize?: number, lineHeight?: number },
+) => {
+  if (!direction) return '';
+
+  let icon = '';
+
+  icon += '<div class="text-center">';
+  icon += `<img src="./icons/${direction}.svg" width="${params?.iconWidth || 24}" />`;
+  icon += '</div>';
+  icon += `<div class="full-width text-center q-mb-sm" style="font-size:${params?.labelSize || 11}px;line-height:${params?.lineHeight || 14}px">${
+    DIRECTIONS.find((currentDirection) => currentDirection.value === direction)?.label
+  }</div>`;
+
+  return icon;
+};
+
+export const getStrengthIcon = (
+  strength: string,
+  params?: { iconWidth?: number, labelSize?: number, lineHeight?: number },
+) => {
+  if (!strength) return '';
+
+  let icon = '';
+
+  icon += '<div class="text-center">';
+  icon += `<img src="./icons/${strength}.svg" width="${params?.iconWidth || 24}" />`;
+  icon += '</div>';
+
+  icon += `<div class="full-width text-center q-mb-sm" style="font-size:${params?.labelSize || 11}px;line-height:${params?.lineHeight || 14}px">${
+    STRENGTH.find((currentStrength) => currentStrength.value === strength)?.label
+  }</div>`;
+
+  return icon;
+};
