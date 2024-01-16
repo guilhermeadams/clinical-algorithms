fd680240f146e90d5378e55887a57ed0b32fc965
Gabriel <gabriel@gabrielsilveira.com.br>
2024-01-16 11:06:22 -0300

    Hotfix: reset editor scrolling while opening graph

--- frontend/src/services/editor/graph.ts
+++ frontend/src/services/editor/graph.ts
@@ -84,6 +84,11 @@ class Graph {
           this.editor.element.textarea.setValues(allElements);
           this.editor.element.textarea.createEventHandlers();
 
+          // reset scroll because of createEventHandlers method
+          // that focus on input fields
+          // which changes the scroll
+          Editor.setScroll({ x: 0, y: 0 });
+
           // READ ONLY MODE
           if (this.editor.data.readOnly) {
             this.editor.element.textarea.disableAll();
